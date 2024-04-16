17a31fa7610da956911c2a381bcae7a746ec6efc
Leonardo <leonardo3442@hotmail.com>
2024-04-16 14:00:20 -0300

    feat: Company and Job implementation

---
+++ src/main/java/br/com/amaral/gestao_vagas/modules/company/useCases/CreateCompanyUseCase.java
@@ -0,0 +1,25 @@
+package br.com.amaral.gestao_vagas.modules.company.useCases;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.amaral.gestao_vagas.exceptions.UserFoundException;
+import br.com.amaral.gestao_vagas.modules.company.entities.CompanyEntity;
+import br.com.amaral.gestao_vagas.modules.company.repositories.CompanyRepository;
+
+@Service
+public class CreateCompanyUseCase {
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    public CompanyEntity execute(CompanyEntity company) {
+        this.companyRepository
+                .findByUsernameOrEmail(company.getUsername(), company.getEmail())
+                .ifPresent((data) -> {
+                    throw new UserFoundException();
+                });
+
+        return this.companyRepository.save(company);
+    }
+}
