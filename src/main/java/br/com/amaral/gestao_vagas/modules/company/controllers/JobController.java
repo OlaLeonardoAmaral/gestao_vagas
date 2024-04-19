20e1d86d22961552f0e7de209e7e196350b96932
Leonardo <leonardo3442@hotmail.com>
2024-04-19 09:16:33 -0300

    Add company id to job creation request, update JWT expiration time, and add security filter for company authentication

--- src/main/java/br/com/amaral/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/amaral/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,12 +1,16 @@
 package br.com.amaral.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.amaral.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.amaral.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.amaral.gestao_vagas.modules.company.useCases.CreateJobUseCase;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,8 +25,17 @@ public class JobController {
     private CreateJobUseCase createJobUseCase;
 
     @PostMapping
-    public ResponseEntity<Object> creaEntity(@Valid @RequestBody JobEntity jobEntity) {
+    public ResponseEntity<Object> creaEntity(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request) {
         try {
+            // getAtribut setado no SecurityFilter -> request.setAttribute("company_id", subjectToken);
+            var companyId = request.getAttribute("company_id");
+            JobEntity jobEntity = JobEntity.builder()
+                                .companyId(UUID.fromString(companyId.toString()))
+                                .description(createJobDTO.getDescription())
+                                .benefits(createJobDTO.getBenefits())
+                                .level(createJobDTO.getLevel())
+                                .build();
+
             var result = this.createJobUseCase.execute(jobEntity);
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
